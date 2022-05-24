962df995de2127cfcce801930587b53b21066724
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-24 15:41:29 +0200

    add attributes Theory

--- Library.Tests/BmiDeterminatorTests.cs
+++ Library.Tests/BmiDeterminatorTests.cs
@@ -2,13 +2,34 @@
 {
     public class BmiDeterminatorTests
     {
-        [Fact]
-        public void DetermineBmi_ForBmiBelow18_5_ReturnsUnderweightClassification()
+        [Theory]
+        [InlineData(10.0)]
+        [InlineData(17.0)]
+        [InlineData(18.0)]
+        [InlineData(8.0)]
+        [InlineData(1.0)]
+        public void DetermineBmi_ForBmiBelow18_5_ReturnsUnderweightClassification(double bmi)
         {
+            //How not doing
+            ////arrange
+
+            //BmiDeterminator bmiDeterminator = new BmiDeterminator();
+            //double[] bmis = new double[] { 5, 10, 15, 18};
+
+            ////act
+            //foreach (var bmi in bmis)
+            //{
+            //    BmiClassification result = bmiDeterminator.DetermineBmi(bmi);
+
+            //    //assert
+
+            //    Assert.Equal(BmiClassification.Underweight, result);
+            //}
+
+            //How doing
             //arrange
 
             BmiDeterminator bmiDeterminator = new BmiDeterminator();
-            double bmi = 10;
 
             //act
 
