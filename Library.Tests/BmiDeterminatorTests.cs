64549944946c4100c479d4814fbcc2d3e07012f8
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-24 17:02:33 +0200

    test parameterization

--- Library.Tests/BmiDeterminatorTests.cs
+++ Library.Tests/BmiDeterminatorTests.cs
@@ -3,12 +3,21 @@
     public class BmiDeterminatorTests
     {
         [Theory]
-        [InlineData(10.0)]
-        [InlineData(17.0)]
-        [InlineData(18.0)]
-        [InlineData(8.0)]
-        [InlineData(1.0)]
-        public void DetermineBmi_ForBmiBelow18_5_ReturnsUnderweightClassification(double bmi)
+        [InlineData(5.0, BmiClassification.Underweight)]
+        [InlineData(10.0, BmiClassification.Underweight)]
+        [InlineData(18.0, BmiClassification.Underweight)]
+        [InlineData(18.9, BmiClassification.Normal)]
+        [InlineData(19.0, BmiClassification.Normal)]
+        [InlineData(24.8, BmiClassification.Normal)]
+        [InlineData(25.1, BmiClassification.Overweight)]
+        [InlineData(27.8, BmiClassification.Overweight)]
+        [InlineData(29.8, BmiClassification.Overweight)]
+        [InlineData(34.8, BmiClassification.Obesity)]
+        [InlineData(32.8, BmiClassification.Obesity)]
+        [InlineData(34.9, BmiClassification.Obesity)]
+        [InlineData(44.9, BmiClassification.ExtremObesity)]
+        [InlineData(89.9, BmiClassification.ExtremObesity)]
+        public void DetermineBmi_ForGivenBmi_ReturnsCorectClassification(double bmi, BmiClassification classification)
         {
             //arrange
 
@@ -20,7 +29,7 @@
 
             //assert
 
-            Assert.Equal(BmiClassification.Underweight, result);
+            Assert.Equal(classification, result);
         }
     }
 }
