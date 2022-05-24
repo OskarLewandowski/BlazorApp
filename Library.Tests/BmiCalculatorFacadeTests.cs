d59daa4f2fef534bd470fafa9bdffa8d10e490d9
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-24 20:05:24 +0200

    refactor test with FluentAssertions

--- Library.Tests/BmiCalculatorFacadeTests.cs
+++ Library.Tests/BmiCalculatorFacadeTests.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 
 namespace Library.Tests
 {
     public class BmiCalculatorFacadeTests
     {
+        private const string OBESITY_SUMARRY = "You should take care of your obesity";
+        private const string NORMAL_SUMARRY = "Your weight is normal, keep it up";
+        private const string OVERWEIGHT_SUMARRY = "You are a bit overweight";
+
         [Theory]
-        [InlineData(100, 170, 34.6, BmiClassification.Obesity)]
-        [InlineData(57, 170, 19.72, BmiClassification.Normal)]
-        [InlineData(70, 170, 24.22, BmiClassification.Normal)]
-        [InlineData(77, 160, 30.08, BmiClassification.Obesity)]
-        [InlineData(80, 190, 22.16, BmiClassification.Normal)]
-        [InlineData(90, 190, 24.93, BmiClassification.Overweight)]
-        public void GetResult_ForValidInputs_ReturnsCorrectRessult(double weight, double height, double bmi, BmiClassification classification)
+        [InlineData(100, 170, 34.6, BmiClassification.Obesity, OBESITY_SUMARRY)]
+        [InlineData(57, 170, 19.72, BmiClassification.Normal, NORMAL_SUMARRY)]
+        [InlineData(70, 170, 24.22, BmiClassification.Normal, NORMAL_SUMARRY)]
+        [InlineData(77, 160, 30.08, BmiClassification.Obesity, OBESITY_SUMARRY)]
+        [InlineData(80, 190, 22.16, BmiClassification.Normal, NORMAL_SUMARRY)]
+        [InlineData(90, 190, 24.93, BmiClassification.Overweight, OVERWEIGHT_SUMARRY)]
+        public void GetResult_ForValidInputs_ReturnsCorrectRessult(double weight, double height, double bmi, BmiClassification classification, string summary)
         {
             //arrange
             IBmiDeterminator bmiDeterminator = new BmiDeterminator();
@@ -27,9 +32,11 @@ namespace Library.Tests
             BmiResult result = bmiCalculatorFacade.GetResult(weight, height);
 
             //assert
+            //FluentAssertions
 
-            Assert.Equal(bmi, result.Bmi);
-            Assert.Equal(classification, result.BmiClassification);
+            result.Bmi.Should().Be(bmi);
+            result.BmiClassification.Should().Be(classification);
+            result.Summary.Should().Be(summary);
         }
     }
 }
