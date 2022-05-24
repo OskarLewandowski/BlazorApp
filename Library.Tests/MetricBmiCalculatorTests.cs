998af49f0e59599632d3924e9aa80fd56080214b
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-24 17:29:09 +0200

    add MetricBmiCalculatorTests

---
+++ Library.Tests/MetricBmiCalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using Library.Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tests
+{
+    public class MetricBmiCalculatorTests
+    {
+        [Theory]
+        [InlineData(100, 170, 34.6)]
+        [InlineData(57, 170, 19.72)]
+        [InlineData(70, 170, 24.22)]
+        [InlineData(77, 160, 30.08)]
+        [InlineData(80, 190, 22.16)]
+        [InlineData(90, 190, 24.93)]
+        public void CalculateBmi_ForGivenWeightAndHeight_ReturnsCorrectBmi(double weight, double height, double bmiResult)
+        {
+            //arrange
+            MetricBmiCalculator metricBmiCalculator = new MetricBmiCalculator();
+
+            //act
+            double result = metricBmiCalculator.CalculateBmi(weight, height);
+
+            //assert
+
+            Assert.Equal(bmiResult, result);
+
+        }
+    }
+}
