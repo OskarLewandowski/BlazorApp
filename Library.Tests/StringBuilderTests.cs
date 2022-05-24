79e21240960e820fa5ca42cee8fa7fe3c276c9be
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-24 14:48:23 +0200

    one assert

--- Library.Tests/StringBuilderTests.cs
+++ Library.Tests/StringBuilderTests.cs
@@ -8,19 +8,22 @@ namespace Library.Tests
             // arrange
 
             StringBuilder sb = new StringBuilder();
+            //StringBuilder sb2 = new StringBuilder();
 
             // act
 
             sb.Append("test")
               .Append("Test2");
-
             sb.Append(".cs");
-
             string result = sb.ToString();
+
+            //sb2.Append("a").Append("b");
+            //string result2 = sb2.ToString();
 
             // assert
 
             Assert.Equal("testTest2.cs", result);
+            //Assert.Equal("ab", result2);
         }
     }
 }
