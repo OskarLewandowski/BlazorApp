858ebe3140d8e68a3a0057c1188c673ec4753e5c
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-24 15:41:57 +0200

    clean code

--- Library.Tests/BmiDeterminatorTests.cs
+++ Library.Tests/BmiDeterminatorTests.cs
@@ -10,23 +10,6 @@
         [InlineData(1.0)]
         public void DetermineBmi_ForBmiBelow18_5_ReturnsUnderweightClassification(double bmi)
         {
-            //How not doing
-            ////arrange
-
-            //BmiDeterminator bmiDeterminator = new BmiDeterminator();
-            //double[] bmis = new double[] { 5, 10, 15, 18};
-
-            ////act
-            //foreach (var bmi in bmis)
-            //{
-            //    BmiClassification result = bmiDeterminator.DetermineBmi(bmi);
-
-            //    //assert
-
-            //    Assert.Equal(BmiClassification.Underweight, result);
-            //}
-
-            //How doing
             //arrange
 
             BmiDeterminator bmiDeterminator = new BmiDeterminator();
