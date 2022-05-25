414c8704cd7ce926088b767b0e5c4333852ebcf9
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-25 15:50:08 +0200

    test using ClassData

---
+++ Exercise.Tests/ValidatorTestsV3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Tests
+{
+    public class ValidatorTestsV3
+    {
+        [Theory]
+        [ClassData(typeof(ValidatorTestsData))]
+        public void ValidateOverlapping_ForOverlappingDateRanges_ReturnsFalsev2(List<DateRange> ranges)
+        {
+            //arrange
+            DateRange input = new DateRange(new DateTime(2020, 1, 10), new DateTime(2020, 1, 20));
+            Validator validator = new Validator();
+
+            //act
+
+            bool result = validator.ValidateOverlapping(ranges, input);
+
+            //assert
+
+            result.Should().BeFalse();
+        }
+    }
+}
