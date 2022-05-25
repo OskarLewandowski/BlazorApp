5c2a34cc50be9242ef08a302bf4018e37669dea6
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-25 15:27:53 +0200

    refactor tests using MemberData

---
+++ Exercise.Tests/ValidatorTestsV2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Tests
+{
+    public class ValidatorTestsV2
+    {
+        private static IEnumerable<object[]> GetRangeList()
+        {
+            yield return new object[]
+            {
+                new List<DateRange>()
+                {
+                    new DateRange(new DateTime(2020, 1, 1), new DateTime(2020, 1, 15)),
+                    new DateRange(new DateTime(2020, 2, 1), new DateTime(2020, 2, 15)),
+                }
+            };
+            yield return new object[]
+            {
+                new List<DateRange>()
+                {
+                    new DateRange(new DateTime(2020, 1, 15), new DateTime(2020, 1, 25)),
+                }
+            };
+            yield return new object[]
+            {
+                new List<DateRange>()
+                {
+                    new DateRange(new DateTime(2020, 1, 8), new DateTime(2020, 1, 25)),
+                }
+            };
+            yield return new object[]
+            {
+                new List<DateRange>()
+                {
+                    new DateRange(new DateTime(2020, 1, 12), new DateTime(2020, 1, 14)),
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetRangeList))]
+        public void ValidateOverlapping_ForOverlappingDateRanges_ReturnsFalsev2(List<DateRange> ranges)
+        {
+            //arrange
+            DateRange input = new DateRange(new DateTime(2020, 1, 10), new DateTime(2020, 1, 20));
+            Validator validator = new Validator();
+
+            //act
+
+            bool result = validator.ValidateOverlapping(ranges, input);
+
+            //assert
+
+            result.Should().BeFalse();
+        }
+    }
+}
---
+++ Library.Tests/MetricBmiCalculatorTestsV2.cs
@@ -0,0 +1,38 @@
+﻿using Library.Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tests
+{
+    public class MetricBmiCalculatorTestsV2
+    {
+        public static IEnumerable<object[]> GetSampleData()
+        {
+            yield return new object[] { 100, 170, 34.6 };
+            yield return new object[] { 57, 170, 19.72 };
+            yield return new object[] { 70, 170, 24.22 };
+            yield return new object[] { 77, 160, 30.08 };
+            yield return new object[] { 80, 190, 22.16 };
+            yield return new object[] { 90, 190, 24.93 };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetSampleData))]
+        public void CalculateBmi_ForGivenWeightAndHeight_ReturnsCorrectBmi(double weight, double height, double bmiResult)
+        {
+            //arrange
+            MetricBmiCalculator metricBmiCalculator = new MetricBmiCalculator();
+
+            //act
+            double result = metricBmiCalculator.CalculateBmi(weight, height);
+
+            //assert
+
+            Assert.Equal(bmiResult, result);
+
+        }
+    }
+}
