7a7b6ec566ed77e9fcac3077bc5d0fc459a04942
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-25 12:26:59 +0200

    add solve of exercise one

--- Exercise.Tests/StringHelperTests.cs
+++ Exercise.Tests/StringHelperTests.cs
@@ -2,10 +2,60 @@ namespace Exercise.Tests
 {
     public class StringHelperTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("Alice have cat", "cat have Alice")]
+        [InlineData("test is test", "test is test")]
+        [InlineData("big car and small truck", "truck small and car big")]
+        [InlineData("This is Example opinion", "opinion Example is This")]
+        [InlineData("12345-67890", "12345-67890")]
+        [InlineData("12345  67890", "67890  12345")]
+        public void ReverseWords_ForGivenSentence_ReturnsReversedSentence(string sentence, string correctSentence)
         {
+            //arrange
 
+            //act
+
+            string result = StringHelper.ReverseWords(sentence);
+
+            //assert
+            //Assert.Equal(correctSentence, result);
+
+            result.Should().Be(correctSentence);
+        }
+
+        [Theory]
+        [InlineData("ala")]
+        [InlineData("KamilœlimaK")]
+        [InlineData("aLa")]
+        [InlineData("KAJAK")]
+        [InlineData("pop")]
+        public void IsPalindrome_ForAPalindromWord_RetrunsTrueValue(string word)
+        {
+            //arrange
+
+            //act
+
+            var result = StringHelper.IsPalindrome(word);
+
+            //assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("ola")]
+        [InlineData("Ala")]
+        [InlineData("Kuba")]
+        [InlineData("Oskar")]
+        [InlineData("test")]
+        public void IsPalindrome_ForANonPalindromWord_RetrunsFalseValue(string word)
+        {
+            //arrange
+
+            //act
+            var result = StringHelper.IsPalindrome(word);
+
+            //assert
+            result.Should().BeFalse();
         }
     }
 }
